92120426860dfeb6a03314587aebf349ffe08024
Mussamakame <mussamakame506@gmail.com>
2022-07-08 17:33:15 +0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbodComponent } from './dashbod/dashbod/dashbod.component';
+import { ActivitiesComponent } from './folder/activities/activities.component';
+import { DashbordComponent } from './folder/dashbord/dashbord.component';
+import { HomeComponent } from './folder/home/home.component';
+import { LogoutComponent } from './folder/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',component:HomeComponent
+},
+
+{
+  path:'dashbod',component:DashbodComponent,
+  children:[
+    {
+      path:'',component:DashbordComponent
+    },
+    {
+      path:'dashbod/dashbord',component:DashbordComponent
+    },
+    {
+      path:'dashbod/activities',component:ActivitiesComponent
+      
+    },
+    {
+      path:'dashbod/dashbord',component:DashbordComponent
+    },
+    {
+      path:'dashbod/',component:HomeComponent
+    }
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
